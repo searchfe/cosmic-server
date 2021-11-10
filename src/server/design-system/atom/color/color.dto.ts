55356f6e2b6d304669b5e8f8343705260cbfb6f6
biyingshuai <biyingshuai@gmail.com>
2021-11-10 16:19:13 +0800

    feat: color module

--- src/server/design-system/atom/color/color.dto.ts
+++ src/server/design-system/atom/color/color.dto.ts
@@ -1,10 +1,34 @@
-import { InputType } from '@nestjs/graphql';
-import { ColorString } from './color.scalar';
+import { InputType, Field, ID } from '@nestjs/graphql';
+
+@InputType()
+export class CreateColorVariantDTO {
+    @Field()
+    light: string;
+
+    @Field()
+    dark: string;
+}
 
 @InputType()
 export class CreateColorDTO {
     /**
      * support rgb、rgba、hex string
      */
-    color: ColorString;
+    @Field()
+    color: string;
+
+    @Field(() => CreateColorVariantDTO, { nullable: true })
+    variant?:  CreateColorVariantDTO;
+
+    @Field(() => ID, { nullable: true })
+    team?: string;
+}
+
+@InputType()
+export class QueryColorDTO {
+    @Field({ nullable: true })
+    id?: string;
+
+    @Field(() => ID, { nullable: true })
+    team?: string;
 }
