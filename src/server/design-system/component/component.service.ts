b7b74e177debd6d5ae884216d838600ad4126e7c
木杉 <zhmushan@qq.com>
2022-03-15 16:14:07 +0800

    feat: Support query components (#19)

---
+++ src/server/design-system/component/component.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { Component } from './schema/component.schema';
+import { QueryComponentDTO } from './schema/component.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ComponentService {
+    constructor(
+        @InjectModel(Component.name)
+        private readonly componentModel: Model<Component>
+    ){}
+
+    async findOne(component: Partial<QueryComponentDTO>) {
+        return await this.componentModel.findOne(component).exec();
+    }
+}
--- src/server/design-system/design-system.module.ts
+++ src/server/design-system/design-system.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { DesignSystemResolver } from './design-system.resolver';
 import { DesignSystemService } from './design-system.service';
 import { AtomModule } from './atom/atom.module';
+import { ComponentModule } from './component/component.module';
 
 @Module({
     imports: [
         AtomModule,
+        ComponentModule,
     ],
     providers: [DesignSystemResolver, DesignSystemService]
 })
