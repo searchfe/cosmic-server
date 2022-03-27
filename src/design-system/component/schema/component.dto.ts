1d0be36e4eba0ecf793c27daba76f12a8cfacefe
木杉 <zhmushan@qq.com>
2022-03-27 16:18:55 +0800

    feat: Component Scheme, DTO, Service, Resolver extends from Base (#29)
    * feat: 支持组件的增删改查

--- src/design-system/component/schema/component.dto.ts
+++ src/design-system/component/schema/component.dto.ts
@@ -1,8 +1,24 @@
-import { InputType, Field } from '@nestjs/graphql';
+import { CreateBaseDTO } from '@/design-system/common/module/base.dto';
+import { InputType, Field, ObjectType, ID } from '@nestjs/graphql';
+import { Types } from 'mongoose';
+import { KeyNodeType } from './component.schema';
 
-
+@ObjectType()
 @InputType()
-export class Property {
+export class CreateKeyNode {
+    @Field()
+    type: KeyNodeType;
+
+    @Field()
+    name: string;
+
+    @Field()
+    desc: string;
+}
+
+@ObjectType()
+@InputType()
+export class CreateProperty {
     @Field()
     name: string;
 
@@ -16,20 +32,44 @@ export class Property {
     desc: string;
 }
 
+@ObjectType()
 @InputType()
-export class CreateComponentDTO {
+export class CreateVariant {
     @Field()
     name: string;
 
     @Field()
+    type: string;
+
+    @Field()
+    defaultValue: string;
+
+    @Field()
+    desc: string;
+}
+@InputType()
+export class CreateComponentDTO extends CreateBaseDTO {
+    @Field()
+    displayName: string;
+
+    @Field()
     desc: string;
 
-    @Field(() => [Property])
-    properties: Property[]
+    @Field(() => [CreateKeyNode])
+    keyNodes: CreateKeyNode[];
+
+    @Field(() => [CreateProperty])
+    properties: CreateProperty[];
+
+    @Field(() => [CreateVariant])
+    variants: CreateVariant[];
 }
 
 @InputType()
-export class QueryComponentDTO extends CreateComponentDTO {
+export class QueryComponentDTO {
+    @Field(() => ID)
+    team: Types.ObjectId;
+
     @Field()
-    id: string;
+    name: string;
 }
--- src/design-system/component/schema/component.schema.ts
+++ src/design-system/component/schema/component.schema.ts
@@ -1,43 +1,85 @@
-import { Field, ObjectType } from '@nestjs/graphql';
+import { BaseSchema } from '@/design-system/common/module/base.schema';
+import { Field, ObjectType, registerEnumType } from '@nestjs/graphql';
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 
 @ObjectType()
 export class Property {
-    @Field(() => String)
+    @Field()
+    name: string;
+
+    @Field()
+    type: string;
+
+    @Field()
+    defaultValue: string;
+
+    @Field()
+    desc: string;
+}
+
+@ObjectType()
+export class Variant {
+    @Field()
     @Prop({ maxlength: 32, required: true, unique: true })
     name: string;
 
-    @Field(() => String)
+    @Field()
     @Prop({ maxlength: 128, required: true })
     type: string;
 
-    @Field(() => String)
+    @Field()
     @Prop({ maxlength: 128, required: true })
     defaultValue: string;
 
-    @Field(() => String)
+    @Field()
+    @Prop({ maxlength: 128, required: true })
+    desc: string;
+}
+
+export enum KeyNodeType {
+    SLOT = 'Slot',
+    TEXT = 'Text',
+};
+
+registerEnumType(KeyNodeType, { name: 'KeyNodeType' });
+
+@ObjectType()
+export class KeyNode {
+    @Field()
+    @Prop({ required: true })
+    type: KeyNodeType;
+
+    @Field()
+    @Prop({ maxlength: 32, required: true })
+    name: string;
+
+    @Field()
     @Prop({ maxlength: 128, required: true })
     desc: string;
 }
 
 @ObjectType()
-@Schema({ timestamps: true })
-export class Component {
-    @Field(() => String)
-    id: string;
+export class Component extends BaseSchema {
+    @Field()
+    @Prop({ maxlength: 32 })
+    displayName: string;
 
-    @Field(() => String)
-    @Prop({ maxlength: 32, required: true })
-    name: string;
-
-    @Field(() => String)
+    @Field()
     @Prop({ maxlength: 128 })
     desc: string;
 
+    @Field(() => [KeyNode])
+    @Prop()
+    keyNodes: KeyNode[];
+
     @Field(() => [Property])
     @Prop()
-    properties: Property[]
+    properties: Property[];
+
+    @Field(() => [Variant])
+    @Prop()
+    variants: Variant[];
 }
 
 export const ComponentSchema = SchemaFactory.createForClass(Component);
