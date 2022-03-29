8881052ad3d0b63e8d1fa04b3070dc271b26fd85
bys <biyingshuai@gmail.com>
2022-03-29 11:29:32 +0800

    feat: 🎸 draft add project FK

--- src/workbench/draft/schema/draft.schema.ts
+++ src/workbench/draft/schema/draft.schema.ts
@@ -1,10 +1,15 @@
-import { ObjectType } from '@nestjs/graphql';
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Field, ObjectType, ID } from '@nestjs/graphql';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { BaseSchema } from '../../../common/module/base.schema';
+import { Types } from 'mongoose';
 
 
 @Schema({ timestamps: true })
 @ObjectType()
-export class Draft extends BaseSchema {}
+export class Draft extends BaseSchema {
+    @Field(() => ID)
+    @Prop({ auto: false, required: true })
+    project: Types.ObjectId;
+}
 
 export const DraftSchema = SchemaFactory.createForClass(Draft);
