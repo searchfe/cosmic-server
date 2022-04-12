aff0eccacfcca394d73cbb77dd8c15429578d4c5
bys <43025123+biyingshuai@users.noreply.github.com>
2022-04-12 17:45:17 +0800

    refactor: 💡 remove fields (#43)

--- src/common/module/base.resolver.ts
+++ src/common/module/base.resolver.ts
@@ -15,14 +15,8 @@ export interface IBaseResolver<TSchema extends BaseSchema> {
     create: (createInput: DeepPartial<TSchema>) => Promise<Class<TSchema>>;
     update: (updateInput: DeepPartial<TSchema>) => Promise<boolean>;
     delete: (id: string) => Promise<boolean>;
-    findAll: (
-        query?: DeepPartial<TSchema>,
-        fields?: Array<keyof TSchema>,
-    ) => Promise<Class<TSchema>[]>;
-    findOne: (
-        id: string,
-        fields?: Array<keyof TSchema>,
-    ) => Promise<Class<TSchema>>;
+    findAll: (query?: DeepPartial<TSchema>) => Promise<Class<TSchema>[]>;
+    findOne: (id: string) => Promise<Class<TSchema>>;
 }
 
 export function BaseResolver<
@@ -90,21 +84,17 @@ export function BaseResolver<
         @Query(() => schema, { name: `get${capitalizeName}`, nullable: true })
         async findOne(
             @Args('id', { type: () => String })
-            id: string,
-            @Args({ type: () => [String], name: 'fields', nullable: true })
-            fields?: Array<keyof TSchema>,
+            id: string
         ) {
-            return await this.dataService.findOne(id, fields);
+            return await this.dataService.findOne(id);
         }
 
         @Query(() => [schema], { name: `${lowerCaseName}s` })
         async findAll(
             @Args({ type: () => queryInput, name: 'query', nullable: true })
-            query?: TCreateInput,
-            @Args({ type: () => [String], name: 'fields', nullable: true })
-            fields?: Array<keyof TSchema>,
+            query?: TCreateInput
         ) {
-            return await this.dataService.findAll(query, fields);
+            return await this.dataService.findAll(query);
         }
 
         @Subscription(() => schema, { name: createTrigger })
